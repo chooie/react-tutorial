405e14cd43e2334aa1a77dc42767a1399cd2efee
Charlie Hebert <charlie.hebert92@gmail.com>
2017-03-20 23:56:34 -0600

    Move game win/click logic to own functions

--- src/js/components/TicTacToeThreeByThree.js
+++ src/js/components/TicTacToeThreeByThree.js
@@ -20,7 +20,8 @@ export default class TicTacToeThreeByThree extends React.Component {
     const current = history[this.state.stepNumber];
     const squaresCopy = current.squares.slice();
 
-    if (calculateWinner(squaresCopy) || squaresCopy[clickedSquareIndex]) {
+    if (thereIsAWinner(squaresCopy) ||
+        theSquareIsFilled(squaresCopy, clickedSquareIndex)) {
       return;
     }
 
@@ -69,6 +70,35 @@ export default class TicTacToeThreeByThree extends React.Component {
   }
 }
 
+function thereIsAWinner(squares) {
+  return calculateWinner(squares);
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function theSquareIsFilled(squares, clickedSquareIndex) {
+  return squares[clickedSquareIndex];
+}
+
+
 function getStatusBasedOnWhetherThereIsAWinner(winner, nextPlayer) {
   if (winner) {
     status = "Winner: " + winner;
@@ -97,24 +127,4 @@ function getNextPlayerSymbol(xIsNext) {
 
 function xIsNextIfStepIsOdd(step) {
   return (step % 2) ? false: true
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
